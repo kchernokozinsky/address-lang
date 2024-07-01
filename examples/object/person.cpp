37d354610eb916a68186f2f5939739a356ed2c2c
kchernokozinsky <kyrylo.chornokozynskyi@ukma.edu.ua>
2024-07-01 22:30:08 +0300

    Update person.cpp

--- examples/object/person.cpp
+++ examples/object/person.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <string>
+
 struct Person {
     std::string* name;
     int* age;
@@ -12,34 +15,34 @@ struct Person {
     }
 };
 
-void say_hi(Person* person) {
-    if (!person || !person->name) {
+void say_hi(Person** person) {
+    if (!*person || !(*person)->name) {
         std::cout << "Hello, I have no name :(" << std::endl;
     } else {
-        std::cout << "Hello, my name is " << *(person->name) << std::endl;
+        std::cout << "Hello, my name is " << *(*person)->name << std::endl;
     }
 }
 
-void say_age(Person* person) {
-    if (!person || !person->age) {
+void say_age(Person** person) {
+    if (!*person || !(*person)->age) {
         std::cout << "I don't know how old I am :(" << std::endl;
     } else {
-        std::cout << "I'm " << *(person->age) << " years old" << std::endl;
+        std::cout << "I'm " << *(*person)->age << " years old" << std::endl;
     }
 }
 
-void say_who_you_married_on(Person* person) {
-    if (!person || !person->married_on || !person->married_on->name) {
+void say_who_you_married_on(Person** person) {
+    if (!*person || !(*person)->married_on || !(*person)->married_on->name) {
         std::cout << "I'm not married yet" << std::endl;
     } else {
-        std::cout << "I'm married to " << *(person->married_on->name) << std::endl;
+        std::cout << "I'm married to " << *(*person)->married_on->name << std::endl;
     }
 }
 
-void marry(Person* person1, Person* person2) {
-    if (person1 && person2) {
-        person1->married_on = person2;
-        person2->married_on = person1;
+void marry(Person** person1, Person** person2) {
+    if (person1 && *person1 && person2 && *person2) {
+        (*person1)->married_on = *person2;
+        (*person2)->married_on = *person1;
     }
 }
 
@@ -47,12 +50,12 @@ int main() {
     Person* alice = new Person("Alice", 23);
     Person* bob = new Person("Bob", 25);
 
-    say_hi(alice);
-    say_age(alice);
+    say_hi(&alice);
+    say_age(&alice);
 
-    marry(alice, bob);
+    marry(&alice, &bob);
 
-    say_who_you_married_on(alice);
+    say_who_you_married_on(&alice);
 
     delete alice;
     delete bob;
